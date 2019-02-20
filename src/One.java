d8954e3e907a48f16e9636fb4ef9798043c1ed98
Jim Filippou <jimfilippou8@gmail.com>
2019-02-21 00:01:44 +0200

    read data file

--- src/One.java
+++ src/One.java
@@ -1,6 +1,23 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.File;
+import java.util.List;
+
 /**
  * First Exercise
  */
 class One {
+  private static final String FILENAME = "/Users/jimfilippou/Projects/hw1b/data/1.1-sm.txt";
+
+  public static void main(String[] args) {
+    try {
+      File f = new File(FILENAME);
+      List<Integer> list = Utilities.convertFileSequenceToList(f);
+      System.out.println(list.isEmpty());
+    } catch (IOException err) {
+      err.printStackTrace();
+    }
+  }
 
 }
