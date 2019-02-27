0589d3199be3b5a2dea81b3c6eaa8375621c5caf
Jim Filippou <jimfilippou8@gmail.com>
2019-02-28 00:37:26 +0200

    2 read file

--- src/Two.java
+++ src/Two.java
@@ -1,7 +1,17 @@
-/**
- * Second Exercise
- */
+import java.io.IOException;
+import java.io.File;
+import java.util.List;
+
 class Two {
 
-  
+  private static final String FILENAME = "/Users/jimfilippou/Projects/hw1b/data/1.2-sm.txt";
+
+  public static void main(String[] args) {
+    try {
+      File f = new File(FILENAME);
+      List<Integer> list = Utilities.convertFileSequenceToList(f);
+    } catch (IOException err) {
+      err.printStackTrace();
+    }
+  }
 }
