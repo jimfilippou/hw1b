236adaa162ed642908b3174141c9d8145dab2b10
Jim Filippou <jimfilippou8@gmail.com>
2019-03-06 00:57:07 +0200

    attempt

--- src/QuickSort.java
+++ src/QuickSort.java
@@ -1,30 +1,36 @@
+import java.util.Random;
+
 class QuickSort {
 
-  public void quickSort(int arr[], int begin, int end) {
-      if (begin < end) {
-          int partitionIndex = partition(arr, begin, end);
-          quickSort(arr, begin, partitionIndex - 1);
-          quickSort(arr, partitionIndex + 1, end);
-      }
-  }
+    public void quickSort(int arr[], int begin, int end) {
+        if (begin < end) {
+            int partitionIndex = partition(arr, begin, end);
+            quickSort(arr, begin, partitionIndex - 1);
+            quickSort(arr, partitionIndex + 1, end);
+        }
+    }
 
-  private int partition(int arr[], int begin, int end) {
-      int pivot = arr[end];
-      int i = (begin - 1);
+    private int partition(int arr[], int begin, int end) {
+        Random r = new Random();
+        int low = 0;
+        int high = arr.length - 1;
+        int result = r.nextInt(high - low) + low;
+        int pivot = arr[result];
+        int i = (begin - 1);
 
-      for (int j = begin; j < end; j++) {
-          if (arr[j] <= pivot) {
-              i++;
-              int swapTemp = arr[i];
-              arr[i] = arr[j];
-              arr[j] = swapTemp;
-          }
-      }
+        for (int j = begin; j < end; j++) {
+            if (arr[j] <= pivot) {
+                i++;
+                int swapTemp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = swapTemp;
+            }
+        }
 
-      int swapTemp = arr[i + 1];
-      arr[i + 1] = arr[end];
-      arr[end] = swapTemp;
+        int swapTemp = arr[i + 1];
+        arr[i + 1] = arr[end];
+        arr[end] = swapTemp;
 
-      return i + 1;
-  }
+        return i + 1;
+    }
 }
